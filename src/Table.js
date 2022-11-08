ce5a09a85fc20417adb1670a113b0141b88541ef
Mukesh Selvaraj <abirami@ishamistygreen.com>
2022-11-08 23:29:27 +0530

    completed

--- src/Table.js
+++ src/Table.js
@@ -1,5 +1,53 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import React from 'react';
+
+export default ({ payments, className }) => {
+  let output = payments
+    .filter((year, i) => i > 0 && (year.balance > 0 || year.interestYearly > 0))
+    .reduce(
+      (acc, year, index) => ({
+        interestTotal: acc.interestTotal + year.interestYearly,
+        overpaymentTotal: acc.overpaymentTotal + year.overpayment,
+        rows: [
+          ...acc.rows,
+          [
+            year.partial ? year.partial + 'm' : index + 1,
+            Math.round(year.interestYearly || 0),
+            Math.round(year.overpayment),
+            Math.round(year.balance),
+          ],
+        ],
+      }),
+      { interestTotal: 0, overpaymentTotal: 0, rows: [] }
+    );
+
+  return (
+    <table className={className}>
+      <thead>
+        <tr>
+          <th>Years</th>
+          <th>Interest</th>
+          <th>Overpayment</th>
+          <th>Balance</th>
+        </tr>
+      </thead>
+      <tbody>
+        {output.rows.map((row, index) => (
+          <tr key={index}>
+            {row.map((d, i) => (
+              <td key={i}>{d.toLocaleString()}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+      <tfoot>
+        <tr>
+          <td colSpan={2}>
+            {Math.round(output.interestTotal).toLocaleString()}
+          </td>
+          <td>{Math.round(output.overpaymentTotal).toLocaleString()}</td>
+          <td />
+        </tr>
+      </tfoot>
+    </table>
+  );
+};
